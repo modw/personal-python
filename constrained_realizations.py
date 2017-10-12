f668babf58de7fbaf0390e8717e4cf89e279c40a
Marcio <modw@users.noreply.github.com>
2017-10-12 11:22:21 -0400

    Added function to obtain correlated piece only

--- constrained_realizations.py
+++ constrained_realizations.py
@@ -47,7 +47,7 @@ def constrained_alm(input_alm, cl_auto_in, cl_auto_out, cl_cross):
     the two auto power spectra and the cross power spectrum.
     ATTENTION:
      - The way it is currently written, ell_max is given by length of input_alm.
-     - Currently it only works with no monopole or dipole (c1 = c2 = 0 for
+     - Currently it only works with no monopole or dipole (c0 = c1 = 0 for
      every cl given)
     """
     # maximum l from input alm
@@ -68,4 +68,30 @@ def constrained_alm(input_alm, cl_auto_in, cl_auto_out, cl_cross):
     random_complex = np.random.normal(size=len(l_list_nz))\
         + 1j*np.random.normal(size=len(l_list_nz))
     output_alm_real[non_zero] = W1*input_alm_real[non_zero] + W2*random_complex
+    return real_to_complex(output_alm_real)
+
+
+def corr_piece(input_alm, cl_auto_in, cl_cross):
+    """
+    Gives correlated piece of output alm.
+    ATTENTION:
+     - The way it is currently written, ell_max is given by length of input_alm.
+     - Currently it only works with no monopole or dipole (c0 = c1 = 0 for
+     every cl given)
+    """
+    # maximum l from input alm
+    LMAX = hp.Alm.getlmax(len(input_alm))
+    # getting the l list for non zero cls (no monopole or dipole)
+    l_list = hp.Alm.getlm(LMAX)[0]
+    non_zero = l_list > 1
+    l_list_nz = l_list[non_zero]
+    # crabbing the non zero cls and ordering to multiply by the alms
+    cl_auto_in_nz, cl_cross_nz = \
+        cl_auto_in[l_list_nz], cl_cross[l_list_nz]
+    # defining weights to generate the constrained realizations
+    W1 = cl_cross_nz/cl_auto_in_nz
+    # calculating the output alm and returning them in complex basis
+    input_alm_real = complex_to_real(input_alm)
+    output_alm_real = np.zeros(len(input_alm_real), dtype=complex)
+    output_alm_real[non_zero] = W1*input_alm_real[non_zero]
     return real_to_complex(output_alm_real)
