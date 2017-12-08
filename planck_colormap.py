f13fd63595ecd893de89e3ecd524d942dd89d2a0
Marcio <modw@users.noreply.github.com>
2017-12-08 12:16:18 -0500

    planck colormap file added; syntax clean-up

--- planck_colormap.py
+++ planck_colormap.py
@@ -2,18 +2,13 @@
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import numpy as np
-import os
 
-### home dir
 
-cwd = os.getcwd().split('/')
-HOME = cwd[1:cwd.index('Dropbox')]
-HOME = "/"+"/".join(HOME)
-
-### importing colormap and registering it
-cmap_rgb = np.loadtxt(HOME + "/Dropbox/Doutorado/Research/Wiener Filtering/Messenger method code/Planck_T_Colormap_RGB.dat")
-ptmap = mcolors.ListedColormap(cmap_rgb,name='PlanckTMap')
+# importing colormap and registering it
+cmap_rgb = np.loadtxt("./files/Planck_T_Colormap_RGB.dat")
+ptmap = mcolors.ListedColormap(cmap_rgb, name='PlanckTMap')
 plt.register_cmap(cmap=ptmap)
+
 
 def colormap(s='PlanckTMap'):
     """
@@ -25,8 +20,7 @@ def colormap(s='PlanckTMap'):
     Name of the desired colormap. Default is 'PlanckTMap'.
     For non-divergent colormaps the recommended is 'viridis'.
     """
-    #global cm
     cm = plt.get_cmap(s)
-    cm.set_under('w',alpha=0)
+    cm.set_under('w', alpha=0)
     cm.set_bad('gray')
     return cm
